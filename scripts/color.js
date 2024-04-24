5ae6428ba3d0da50a7b8e42b958762460dd6b06e
hemi <57880110+hemisemidemipresent@users.noreply.github.com>
2024-04-24 10:55:09 +0800

    Update color.js

--- scripts/color.js
+++ scripts/color.js
@@ -3,12 +3,28 @@ const colors = {
     WP: ["#faa", "#e85a65", "#ab0000"],
     PSP: ["#FFE680", "#EDBD00"],
     SDP: ["#FFAD72","#FF8025"],
-    NSP: ["#CFE08C","#A2BE32"],
+    NSP: ["#C6DB76","#A2BE32"],
     PV: ["#DBA9E8","#C572DA"],
     Reform: ["#E8DBA9","#CFB44A"],
-    SPP: ["#B6B6FF","#8181FF"],
-    SDA: ["#8CCFE0","#2E93AD"],
-    RDU: ["#FFB6F0", "#FF81E6"],
-    PPP: ["#D7915F", "#A15A28"],
+    RDU: ["#B6B6FF","#8181FF"],
+    SPP: ["#8CCFE0","#2E93AD"],
+    SDA: ["#8aff97","#50c85e", "#1c8c28"],
+    SingFirst: ["#FFB6F0", "#FF81E6"],
+    PPP: ["#DA996B", "#C46E31"],
     Independent:["#ccc","#999"]
-  };
+};
+// const colors = {
+//     PAP: ["#8aafff", "#577ccc", "#2c5aa0", "#12325C"],
+//     WP: ["#ff8b98", "#e85a65", "#ab0000"],
+//     PSP: ["#FFE680", "#EDBD00"],
+//     SDA: ["#8aff97","#50c85e", "#1c8c28"],
+//     SDP: ["#FFAD72","#FF8025"],
+//     NSP: ["#CFE08C","#A2BE32"],
+//     PV: ["#DBA9E8","#C572DA"],
+//     Reform: ["#E8DBA9","#CFB44A"],
+//     SPP: ["#B6B6FF","#8181FF"],
+//     // SDA: ["#8CCFE0","#2E93AD"],
+//     RDU: ["#FFB6F0", "#FF81E6"],
+//     PPP: ["#D7915F", "#A15A28"],
+//     Independent:["#ccc","#999"]
+// };
